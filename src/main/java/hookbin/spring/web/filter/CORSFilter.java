883172e6bd6eaf0b0d22e29eb6ac5d42a5b2fff3
Scott Stevelinck <scott@fit-pay.com>
2015-08-31 10:18:30 -0600

    added cross origin filter to provide cross origin support

---
+++ src/main/java/hookbin/spring/web/filter/CORSFilter.java
@@ -0,0 +1,34 @@
+package hookbin.spring.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {}
+}
